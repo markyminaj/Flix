1f6c7e684bba413a7e0302df7c417f3f3012c461
Mark <menm619@gmail.com>
2022-02-26 01:11:03 -0800

    add gif

---
+++ Flix/MovieCell.swift
@@ -0,0 +1,26 @@
+//
+//  TableViewCell.swift
+//  Flix
+//
+//  Created by Mark Martin on 2/25/22.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
